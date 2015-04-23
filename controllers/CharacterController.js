74e69bdbe07f9e9efbf9b98ea5dd5727b6192b62
Daniel Toader & Cynthia Wilhelm <student@epicodus.com>
2015-04-23 09:59:33 -0700

    fix state transfer

--- controllers/CharacterController.js
+++ controllers/CharacterController.js
@@ -1,4 +1,4 @@
-character.controller('CharacterCtrl', function CharacterCtrl($scope, CharacterFactory) {
+character.controller('CharacterCtrl', function CharacterCtrl($scope, $state, CharacterFactory) {
   $scope.player = CharacterFactory.factory;
   $scope.CharacterFactory = CharacterFactory;
 
@@ -6,7 +6,11 @@ character.controller('CharacterCtrl', function CharacterCtrl($scope, CharacterFa
     var playerName = $scope.playerName;
     var playerSex = $scope.playerSex;
     var playerClass = $scope.playerClass;
+    console.log("name = " + playerName);
+    console.log("sex = " + playerSex);
+    console.log("class = " + playerClass);
     $scope.CharacterFactory.createPlayer(playerName, playerSex, playerClass);
     $scope.playerName = null;
+    $state.go('/game');
   };
 });
