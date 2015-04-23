8cdda4acfbf92d20e3eb9f1b38d18af4c8b82ec5
Daniel Toader & Cynthia Wilhelm <student@epicodus.com>
2015-04-23 10:48:20 -0700

    break everything

--- controllers/CharacterController.js
+++ controllers/CharacterController.js
@@ -1,6 +1,7 @@
 character.controller('CharacterCtrl', function CharacterCtrl($scope, $state, CharacterFactory) {
-  $scope.player = CharacterFactory.factory;
   $scope.CharacterFactory = CharacterFactory;
+  $scope.players = CharacterFactory.factory;
+
 
   $scope.createPlayer = function() {
     var playerName = $scope.playerName;
@@ -10,6 +11,9 @@ character.controller('CharacterCtrl', function CharacterCtrl($scope, $state, Cha
     console.log("sex = " + playerSex);
     console.log("class = " + playerClass);
     $scope.CharacterFactory.createPlayer(playerName, playerSex, playerClass);
+    console.log("name in object = " + players.player[0].playerName);
+    console.log("sex in object = " + players.player[0].playerSex);
+    console.log("class in object = " + players.player[0].playerClass);
     $scope.playerName = null;
     $state.go('/game');
   };
--- services/CharacterFactory.js
+++ services/CharacterFactory.js
@@ -1,10 +1,15 @@
 character.factory('CharacterFactory', function() {
   var factory = {};
+  factory.player = [];
 
   factory.createPlayer = function(playerName, playerSex, playerClass) {
-    factory.playerName = playerName;
-    factory.playerSex = playerSex;
-    factory.playerClass = playerClass;
+    factory.player.push({playerName: playerName, playerSex: playerSex, playerClass: playerClass});
+    // factory.playerName = playerName;
+    // factory.playerSex = playerSex;
+    // factory.playerClass = playerClass;
+    console.log("factory name: " + factory.player[0].playerName);
+    console.log("factory sex: " + factory.player[0].playerSex);
+    console.log("factory class: " + factory.player[0].playerClass);
   };
   return factory;
 });
